dd26e71fc347104f7b38a42683d8e1b3f7442e52
Lan Pham <lanpham312@gmail.com>
2017-11-03 13:35:03 +0000

    Fake Binary

---
+++ ruby/8kyu/fake_binary.rb
@@ -0,0 +1,8 @@
+=begin
+Given a string of digits, you should replace any digit below 5 with '0' and any
+digit 5 and above with '1'. Return the resulting string.
+=end
+
+def fake_bin(s)
+  s.gsub(/[1-4]/, "0").gsub(/[5-9]/, "1")
+end
