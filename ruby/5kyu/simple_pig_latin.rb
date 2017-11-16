b5fc0c4d4c86f9d06619f1486ffc6e5b3612cada
Lan Pham <lanpham312@gmail.com>
2017-11-16 17:27:58 +0000

    Adding 5kyu Simple Pig Latin

---
+++ ruby/5kyu/simple_pig_latin.rb
@@ -0,0 +1,12 @@
+# Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+
+def pig_it(text)
+  text = text.split(" ").map do |item|
+    if item =~ /[[:punct:]]/
+      item
+    else
+      item.chars.rotate(1).push("ay").join("")
+    end
+  end
+  text.join(" ")
+end
